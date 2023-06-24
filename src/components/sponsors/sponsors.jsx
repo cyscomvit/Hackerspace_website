7828da00d153e5e06c05af0b792e8e725a1dd8c8
AdityaSai2004 <2004sai@gmail.com>
2023-06-24 22:38:24 +0530

    Best practice

--- src/components/sponsors/sponsors.jsx
+++ src/components/sponsors/sponsors.jsx
@@ -1,32 +1,24 @@
-import React from "react";
-import "./sponsors.css";
+import React from 'react'
+import './sponsors.css'
 
 const Sponsors = () => {
-  return (
-    <>
-      <div className="center">
-        <div id="logo">
-          CY<span>S</span>COM
-        </div>
-      </div>
-      <div className="sponsors">
-        <div className="span" id="title">
-          SPONSORS
-        </div>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
-          vitae at quod soluta quam. Tempora illum sunt repellat doloribus
-          accusamus.
-        </p>
+    return (
+        <>
+            <div className='center'>
+                <div id="logo">CY<span>S</span>COM</div>
+            </div>
+            <div className='sponsors'>
+                <div className="span" id="title">SPONSORS</div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores vitae at quod soluta quam. Tempora illum sunt repellat doloribus accusamus.</p>
 
-        <div className="cards">
-          <div className="box"> </div>
-          <div className="box"> </div>
-          <div className="box"> </div>
-        </div>
-      </div>
-    </>
-  );
-};
+                <div className="cards">
+                    <div className="box"> </div>
+                    <div className="box"> </div>
+                    <div className="box"> </div>
+                </div>
+            </div>
+        </>
+    )
+}
 
-export default Sponsors;
+export default Sponsors
