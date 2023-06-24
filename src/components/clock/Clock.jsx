0c0a51d9e1ce40e2e20c36fe0cf20d9e021e6e3e
AdityaSai2004 <2004sai@gmail.com>
2023-06-24 22:52:02 +0530

    clock+faq

---
+++ src/components/clock/Clock.jsx
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+import "./Clock.css";
+
+const Clock = ({ timerDays, timerHours, timerMinutes, timerSeconds }) => {
+  return (
+    <Fragment>
+      <div className="timer-container">
+        <div className="timer">
+          <div className="clock">
+            <section className="he">
+              <p>{timerDays}</p>
+              <small>Days</small>
+            </section>
+            <small>:</small>
+
+            <section className="he">
+              <p> {timerHours}</p>
+              <small> Hours </small>
+            </section>
+            <small>:</small>
+
+            <section className="he">
+              <p>{timerMinutes}</p>
+              <small>Minutes</small>
+            </section>
+            <small>:</small>
+
+            <section className="he">
+              <p>{timerSeconds}</p>
+              <small>Seconds</small>
+            </section>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+Clock.defaultProps = {
+  timerDays: 10,
+  timerHours: 10,
+  timerMinutes: 10,
+  timerSeconds: 10,
+};
+export default Clock;
