dc6d7bf9f1ffca4a3db3bdd4d326657fd75304a8
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-06-24 23:07:07 +0530

    Speaker commit after refactoring code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {OrbitControls,useGLTF,useAnimations} from "@react-three/drei";
 import './App.css';
 import Scene from "./Scene";
 import Navbar from './components/navBar/nav'
+import Speaker from './components/speaker/Speakers'
 
 export default function App() {
   return (
@@ -12,7 +13,8 @@ export default function App() {
       {/* <Canvas shadows={true} camera={{fov:100,position:[-1,1,4]}}> */}
         {/* <Scene/> */}
       {/* </Canvas> */}
-      <Navbar currentPage="1" />
+      {/* <Navbar currentPage="1" /> */}
+      <Speaker />
     </div>
   );
 }
