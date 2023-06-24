a6a966cda40adae34ee9dd818073d37b44d686b1
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-06-24 21:38:29 +0530

    Updated navbar prop variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { Canvas,useFrame} from "@react-three/fiber";
 import {OrbitControls,useGLTF,useAnimations} from "@react-three/drei";
 import './App.css';
 import Scene from "./Scene";
+import Navbar from './components/navBar/nav'
 
 export default function App() {
   return (
     <div className="wrapper" style={{ width: "100vw", height: "100vh" }}>
-      <Canvas shadows={true} camera={{fov:100,position:[-1,1,4]}}>
-        <Scene/>
-      </Canvas>
+      {/* for now commenting the model */}
+      {/* <Canvas shadows={true} camera={{fov:100,position:[-1,1,4]}}> */}
+        {/* <Scene/> */}
+      {/* </Canvas> */}
+      <Navbar currentPage="1" />
     </div>
   );
 }
