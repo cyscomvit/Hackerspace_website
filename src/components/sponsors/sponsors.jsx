9ae0ca82446fbbd3a001dd92de8582c9b7c1c089
AdityaSai2004 <2004sai@gmail.com>
2023-06-24 22:27:27 +0530

    added sponsor components

---
+++ src/components/sponsors/sponsors.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./sponsors.css";
+
+const Sponsors = () => {
+  return (
+    <>
+      <div className="center">
+        <div id="logo">
+          CY<span>S</span>COM
+        </div>
+      </div>
+      <div className="sponsors">
+        <div className="span" id="title">
+          SPONSORS
+        </div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+          vitae at quod soluta quam. Tempora illum sunt repellat doloribus
+          accusamus.
+        </p>
+
+        <div className="cards">
+          <div className="box"> </div>
+          <div className="box"> </div>
+          <div className="box"> </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sponsors;
